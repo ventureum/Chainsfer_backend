d3cb99d34001987908329def0a7aaccca5a739f7
EC2 Default User <ec2-user@ip-172-31-40-11.us-west-2.compute.internal>
2019-04-06 04:48:39 +0000

    Optimized backend with email notification

---
+++ lambda/src/stream.js
@@ -0,0 +1,44 @@
+var QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/727151012682/SenderToChainsfer'
+var AWS = require('aws-sdk')
+var sqs = new AWS.SQS({ region: 'us-east-1' })
+var utils = require('./utils.js')
+
+exports.handler = function (event, context, callback) {
+  event.Records.forEach(function (record) {
+    const newImage = record.dynamodb.NewImage
+    const transferStage = newImage.transferStage.S
+    const txState = newImage[utils.lowerCaseFirstLetter(transferStage)].M.txState.S
+    if (txState === 'Pending') {
+      console.log('eventName: %s, transferId: %s, transferStage: %s',
+        record.eventName, newImage.transferId.S, transferStage)
+      const params = {
+        MessageBody: JSON.stringify(newImage),
+        QueueUrl: QUEUE_URL,
+        MessageAttributes: {
+          'RetryCount': {
+            DataType: 'Number',
+            StringValue: '0'
+          },
+          'TxHashConfirmed': {
+            DataType: 'Number',
+            StringValue: '0' // O means False, 1 means true
+          },
+          'GasTxHashConfirmed': {
+            DataType: 'Number',
+            StringValue: '0' // O means False, 1 means true
+          }
+        }
+      }
+      sqs.sendMessage(params, function (err, data) {
+        if (err) {
+          console.log('Fail to send message: ', err)
+          context.done('error', 'ERROR Put SQS')
+        } else {
+          console.log('MessageId:', data.MessageId)
+          context.done(null, '')
+        }
+      })
+    }
+  })
+  callback(null, 'message')
+}

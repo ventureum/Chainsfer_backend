043482a38e0664217c51316d72bad1f6d3c491ee
Timothy Wang <timothywangdev@gmail.com>
2020-03-30 22:13:58 +0000

    Resolve #286 Email template bugs, 2019, emails to names

--- emailTemplates/src/emailTemplateJsonGenerator.js
+++ emailTemplates/src/emailTemplateJsonGenerator.js
@@ -24,70 +24,70 @@ const EMAIL_META = [
   {
     filename: 'send_action_sender_notification.html',
     name: 'sendActionSenderEmail',
-    subject: 'Chainsfr: {{transferAmount}} {{cryptoSymbol}} has been sent to {{destination}}'
+    subject: 'Chainsfr: {{transferAmount}} {{cryptoSymbol}} has been sent to {{receiverName}}'
   },
   {
     filename: 'send_action_receiver_notification.html',
     name: 'sendActionReceiverEmail',
-    subject: 'Chainsfr: {{sender}} sent you {{transferAmount}} {{cryptoSymbol}}'
+    subject: 'Chainsfr: {{senderName}} sent you {{transferAmount}} {{cryptoSymbol}}'
   },
   {
     filename: 'receive_action_sender_notification.html',
     name: 'receiveActionSenderEmail',
     subject:
-      'Chainsfr: {{destination}} accepted your transfer of {{transferAmount}} {{cryptoSymbol}}'
+      'Chainsfr: {{receiverName}} accepted your transfer of {{transferAmount}} {{cryptoSymbol}}'
   },
   {
     filename: 'receive_action_receiver_notification.html',
     name: 'receiveActionReceiverEmail',
     subject:
-      'Chainsfr: A transfer of {{transferAmount}} {{cryptoSymbol}} from {{sender}} has been deposited'
+      'Chainsfr: A transfer of {{transferAmount}} {{cryptoSymbol}} from {{senderName}} has been deposited'
   },
   {
     filename: 'cancel_action_sender_notification.html',
     name: 'cancelActionSenderEmail',
     subject:
-      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} to {{destination}} has been cancelled'
+      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} to {{receiverName}} has been cancelled'
   },
   {
     filename: 'reclaim_action_sender_notification.html',
     name: 'reclaimActionSenderEmail',
     subject:
-      'Chainsfr: The expired transfer of {{transferAmount}} {{cryptoSymbol}} to {{destination}} has been reclaimed'
+      'Chainsfr: The expired transfer of {{transferAmount}} {{cryptoSymbol}} to {{receiverName}} has been reclaimed'
   },
   {
     filename: 'cancel_action_receiver_notification.html',
     name: 'cancelActionReceiverEmail',
     subject:
-      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} from {{sender}} has been cancelled'
+      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} from {{senderName}} has been cancelled'
   },
   {
     filename: 'expire_action_sender_notification.html',
     name: 'expireActionSenderEmail',
     subject:
-      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} to {{destination}} has expired'
+      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} to {{receiverName}} has expired'
   },
   {
     filename: 'expire_action_receiver_notification.html',
     name: 'expireActionReceiverEmail',
     subject:
-      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} from {{sender}} has expired'
+      'Chainsfr: The transfer of {{transferAmount}} {{cryptoSymbol}} from {{senderName}} has expired'
   },
   {
     filename: 'reminder_action_receiver_notification.html',
     name: 'reminderActionReceiverEmail',
     subject:
-      'Chainsfr: Remember to deposit your {{transferAmount}} {{cryptoSymbol}} from {{sender}}'
+      'Chainsfr: Remember to deposit your {{transferAmount}} {{cryptoSymbol}} from {{senderName}}'
   },
   {
     filename: 'reminder_action_sender_notification.html',
     name: 'reminderActionSenderEmail',
     subject:
-      'Chainsfr: Remember to reclaim your {{transferAmount}} {{cryptoSymbol}} to {{destination}}'
+      'Chainsfr: Remember to reclaim your {{transferAmount}} {{cryptoSymbol}} to {{receiverName}}'
   }
 ]
 
-async function main() {
+async function main () {
   let listTemplatesPromise = new AWS.SES({ apiVersion: '2010-12-01' }).listTemplates().promise()
   console.log('Existing templates: ', await listTemplatesPromise)
 

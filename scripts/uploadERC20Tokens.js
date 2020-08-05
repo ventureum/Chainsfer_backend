8165343ab94bddb06a2dfd517226fb6dd0edf463
clzhong <vincentzhongca@gmail.com>
2020-08-05 17:05:30 -0400

    Resolve #398 Use cryptoType as hasKey for EthContractsTable

--- scripts/uploadERC20Tokens.js
+++ scripts/uploadERC20Tokens.js
@@ -7,6 +7,20 @@ import CoinGecko from 'coingecko-api'
 const CoinGeckoClient = new CoinGecko()
 const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 
+const testnetContractAddresses = {
+  '0': '0xD8ddF454AED9454087f8f678BfFCEcd34abc2A92',
+  '2': '0x7f74d45e51a63E6cB71A51dc5c3C7564217e5737',
+  '3': '0xc9a47A3A177795B03417D263B692dA546081F800',
+  '4': '0x89e0Af4348491b71571D4B209FeEA585691f3D81',
+  '6': '0x91449BF3EB78b3DE43eD16bc81cf00154E4df565',
+  '7': '0x88d0E586507AC0bC31C9fcAF998d269a548d7539',
+  '8': '0x7B36c055d4c05eF4A07b37e7C65ECaC1Af89D807',
+  '9': '0x8893EAF132420c4d8B31f76c3d7303d14fb4affa',
+  '10': '0x662197328a1A5202E0a528E13E8b80d7eBd52300',
+  '12': '0x4998560DCe75d41087B9763A0bB5BAf474ca3322',
+  '18': '0x4aacB7f0bA0A5CfF9A8a5e8C0F24626Ee9FDA4a6'
+}
+
 async function uploadERC20Contracts (env: string) {
   if (!env) throw new Error('taget env missing')
   const file = fs.readFileSync(__dirname + '/ERC20Tokens.json')
@@ -19,13 +33,17 @@ async function uploadERC20Contracts (env: string) {
   let putRequests = erc20Contracts.map((contract: EthContractType): {
     PutRequest: { Item: EthContractType }
   } => {
+    let item = {
+      ...contract,
+      erc20: true,
+      cryptoType: symbolIdMap[contract.symbol.toLocaleLowerCase()]
+    }
+    if (env.toLocaleLowerCase() !== 'prod') {
+      item.address = testnetContractAddresses[contract.decimals.toString()]
+    }
     return {
       PutRequest: {
-        Item: {
-          ...contract,
-          erc20: true,
-          cryptoType: symbolIdMap[contract.symbol.toLocaleLowerCase()]
-        }
+        Item: item
       }
     }
   })
